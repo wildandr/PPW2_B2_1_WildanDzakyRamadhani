50e1bb0ac1ef3ef3164eeaf88ab9f44963103688
wildanzake <wildandzaky4@gmail.com>
2023-09-26 13:45:55 +0700

    bug fixing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app/Http/Controllers/BarangController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/barangs', function () {
+    return view('barangs');
 });
