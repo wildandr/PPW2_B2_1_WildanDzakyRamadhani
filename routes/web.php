af682fe453e8d5dcffa5e839addabf652521221f
wildanzake <wildandzaky4@gmail.com>
2023-09-26 13:46:32 +0700

    bug fixing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-use app/Http/Controllers/BarangController;
+use app\Http\Controllers\BarangController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
